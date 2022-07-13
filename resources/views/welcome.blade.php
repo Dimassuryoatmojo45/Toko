e0839b34b54193a739b52b9c3850f8beefbe8a4f
Dimas Suryo Atmojo <107175975+Dimassuryoatmojo45@users.noreply.github.com>
2022-07-13 14:25:28 +0700

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel fggfgf
                 </div>
 
                 <div class="links">
